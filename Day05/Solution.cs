8cbf8f5202292990ea4c3d2b35cc1de26dfb02ba
Rory Collins <rory.collins@hotmail.co.uk>
2023-11-28 16:00:43 +0000

    Add Day Five

---
+++ Day05/Solution.cs
@@ -0,0 +1,71 @@
+namespace Day05;
+
+using Shared;
+
+public class Solution : Solver
+{
+    private readonly IEnumerable<string> input;
+
+    public Solution(IEnumerable<string> input)
+    {
+        this.input = input;
+    }
+
+    public object PartOne()
+    {
+        return input.Count(x => ContainsAtLeastThreeVowels(x) &&
+                                ContainsDoubleLetter(x) &&
+                                !ContainsBannedPart(x));
+    }
+
+    public object PartTwo()
+    {
+
+        return input.Count(x => ContainsDoublePair(x) &&
+                                ContainsMirrorTriple(x));
+    }
+
+    private bool ContainsMirrorTriple(string s)
+    {
+        for (int i = 0; i < s.Length - 2; i++)
+        {
+            if (s[i] == s[i + 2]) return true;
+        }
+
+        return false;
+    }
+
+    private bool ContainsDoublePair(string s)
+    {
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            var part = s.Substring(i, 2);
+            var remainder = s.Substring(i + 2);
+            if (remainder.Contains(part)) return true;
+        }
+
+        return false;
+    }
+
+    private bool ContainsBannedPart(string s)
+    {
+        var bannedParts = new[] { "ab", "cd", "pq", "xy" };
+        return bannedParts.Any(s.Contains);
+    }
+
+    private bool ContainsDoubleLetter(string s)
+    {
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i + 1] == s[i]) return true;
+        }
+
+        return false;
+    }
+
+    private bool ContainsAtLeastThreeVowels(string s)
+    {
+        var vowels = new[]{'a', 'e', 'i', 'o', 'u'};
+        return s.Count(vowels.Contains) >= 3;
+    }
+}
