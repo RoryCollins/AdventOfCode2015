5602f505538e9ca60796e053d466c07ce37e8fac
Rory Collins <rory.collins@hotmail.co.uk>
2023-11-30 12:11:40 +0000

    Finish Day Eleven

--- Day11/Requirement.cs
+++
@@ -1,7 +0,0 @@
-namespace Day11;
-
-public interface Requirement
-{
-    bool IsValid(string password);
-    string IncrementToValidPassword(string password);
-}
--- Day11/Solution.cs
+++ Day11/Solution.cs
@@ -1,73 +1,70 @@
 namespace Day11;
 
+using Shared;
+
 public class Solution
 {
-    private readonly string currentPassword;
-    private string secondPassword;
+    private char[] password;
+    private static readonly char[] BannedLetters = { 'i', 'o', 'l' };
+    private static readonly IEnumerable<char[]> ThreeLetterStraights = "abcdefghijklmnopqrstuvwxyz".Windowed(3);
 
-    public Solution(string currentPassword)
+    public Solution(string password)
     {
-        this.currentPassword = currentPassword;
+        this.password = password.ToArray();
     }
 
     public object PartOne()
     {
-        // secondPassword = GetNextPassword(currentPassword);
-
-        return secondPassword;
+        return FindNextPassword();
     }
-
 
     public object PartTwo()
-    {
-        return new DoubleLetterRequirement().IncrementToValidPassword("dezbb"); //should be dzzbb
-        // var thirdPassword = GetNextPassword(secondPassword);
-
-        // return thirdPassword;
+   {
+        return FindNextPassword();
     }
 
-    private string GetNextPassword(string password)
+    private string FindNextPassword()
     {
-        var nextPassword = IncrementPassword(password);
-        var requirements = new Requirement[]
-        {
-            new DoubleLetterRequirement(),
-            new StraightLetterRequirement(),
-            new BannedLetterRequirement(),
-        };
-        bool changed;
         do
         {
-            changed = false;
-            foreach (var requirement in requirements)
-            {
-                if (!requirement.IsValid(nextPassword))
-                {
-                    nextPassword = requirement.IncrementToValidPassword(nextPassword);
-                    changed = true;
-                    break;
+            Increment(ref this.password);
+        } while (!IsValid(password));
+
+        return string.Join("", password);
+    }
+
+    private static bool BannedLetterCheck(char c) => !(BannedLetters.Contains(c));
+    private static bool StraightLetterCheck(char[] cs) => ThreeLetterStraights.Contains(cs);
+
+    private static bool IsValid(char[] password) {
+        var straightLetterCheck = false;
+        var doubleLetterCheck = false;
+        for (int j = 2; j < password.Length; j++) {
+            if (!BannedLetterCheck(password[j])) return false;
+            if (StraightLetterCheck(password[(j-2)..j])) {
+                straightLetterCheck = true;
+            }
+            if (j <= 2) continue;
+            for (var k = 0; k + 2 < j; k++) {
+                if (password[j - 3 - k] == password[j - 2 - k]
+                    && password[j - 1] == password[j]
+                    && password[j - 2 - k] != password[j - 1]) {
+                    doubleLetterCheck = true;
                 }
             }
-        } while (changed);
-
-        return nextPassword;
-    }
-
-    private string IncrementPassword(string password)
-    {
-        var nextPassword = password.ToArray();
-        for (var i = nextPassword.Length - 1; i >= 0; i--)
-        {
-            if (nextPassword[i] == 'z')
-            {
-                nextPassword[i] = 'a';
-                continue;
-            }
-
-            nextPassword[i] = (char)(nextPassword[i] + 1);
-            break;
         }
-
-        return string.Join("", nextPassword);
+        return straightLetterCheck && doubleLetterCheck;
     }
+
+    private static void Increment(ref char[] password, int at = -1) {
+        if (at == -1) {
+            at = password.Length - 1;
+        }
+        password[at]++;
+        if (password[at] == 'i' || password[at] == 'o' || password[at] == 'l') password[at]++;
+        if (password[at] <= 'z') return;
+        password[at] = 'a';
+        Increment(ref password, at - 1);
+    }
+
 }
